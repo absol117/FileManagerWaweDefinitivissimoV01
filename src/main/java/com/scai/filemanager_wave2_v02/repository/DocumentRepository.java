33918796aceed778d49933959095b5f55fca38d0
luca <lucamossioapple@gmail.com>
2023-09-16 16:32:34 +0200

    Errore permessi d' accesso

--- src/main/java/com/scai/filemanager_wave2_v02/repository/DocumentRepository.java
+++ src/main/java/com/scai/filemanager_wave2_v02/repository/DocumentRepository.java
@@ -2,9 +2,10 @@ package com.scai.filemanager_wave2_v02.repository;
 
 import com.scai.filemanager_wave2_v02.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
     Optional<Document> findDocumentByPath(String path);
 }
